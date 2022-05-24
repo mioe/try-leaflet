44c900832efbb19f349d81e3285ddd1f224ccfaa
mioe <mioe.boss@gmail.com>
2022-05-24 12:34:22 +0300

    wip

---
+++ src/markers.js
@@ -0,0 +1,208 @@
+window.mapMarkers = [
+	{
+		id: 1,
+		name: 'Вилла «Лидия»',
+		coordinates: [872, 1100],
+	},
+	{
+		id: 2,
+		name: 'Вилла «Лика»',
+		coordinates: [1059, 1130],
+	},
+	{
+		id: 3,
+		name: 'Вилла «Татьяна»',
+		coordinates: [676, 1236],
+	},
+	{
+		id: 4,
+		name: 'Вилла «Ольга»',
+		coordinates: [909, 1201],
+	},
+	{
+		id: 5,
+		name: 'Вилла «Нина»',
+		coordinates: [1054, 1226],
+	},
+	{
+		id: 6,
+		name: 'Вилла «Душечка»',
+		coordinates: [661, 1396],
+	},
+	{
+		id: 7,
+		name: 'Вилла «Евдокия»',
+		coordinates: [882, 1367],
+	},
+	{
+		id: 8,
+		name: 'Детская площадка',
+		coordinates: [963, 1401],
+	},
+	{
+		id: 9,
+		name: 'Нимфея Royal Residence',
+		coordinates: [582, 1694],
+	},
+	{
+		id: 10,
+		name: 'SPA-комплекс',
+		coordinates: [210, 1789],
+	},
+	{
+		id: 11,
+		name: '3D-бассейн',
+		coordinates: [224, 1866],
+	},
+	{
+		id: 12,
+		name: 'Спортивный шатер',
+		coordinates: [736, 2383],
+	},
+	{
+		id: 13,
+		name: 'Карта пляжа №4',
+		coordinates: [970, 2467],
+	},
+	{
+		id: 14,
+		name: 'Детский бассейн',
+		coordinates: [924, 2526],
+	},
+	{
+		id: 15,
+		name: 'Песочница',
+		coordinates: [890, 2585],
+	},
+	{
+		id: 16,
+		name: 'Беседка',
+		coordinates: [980, 2630],
+	},
+	{
+		id: 17,
+		name: 'Вилла «Надя»',
+		coordinates: [1466, 1068],
+	},
+	{
+		id: 18,
+		name: 'Вилла «Елена»',
+		coordinates: [1251, 1298],
+	},
+	{
+		id: 19,
+		name: 'Конференц-отель «Анюта»',
+		coordinates: [1397, 1501],
+	},
+	{
+		id: 20,
+		name: 'Ресторан Melodia',
+		coordinates: [1374, 1616],
+	},
+	{
+		id: 21,
+		name: 'LEGO-клуб',
+		coordinates: [1502, 1649],
+	},
+	{
+		id: 22,
+		name: 'Олива 500 лет',
+		coordinates: [1446, 1667],
+	},
+	{
+		id: 23,
+		name: 'Баня на дровах',
+		coordinates: [1241, 2347],
+	},
+	{
+		id: 24,
+		name: 'Большая беседка',
+		coordinates: [1348, 2393],
+	},
+	{
+		id: 25,
+		name: 'Беседка',
+		coordinates: [1492, 2383],
+	},
+	{
+		id: 26,
+		name: 'Карта пляжа №3',
+		coordinates: [1492, 2477],
+	},
+	{
+		id: 27,
+		name: 'Вилла «Яночка»',
+		coordinates: [2175, 1124],
+	},
+	{
+		id: 28,
+		name: 'Часовня им. Сергия Радонежского',
+		coordinates: [1823, 1322],
+	},
+	{
+		id: 29,
+		name: 'Вилла «Кораллы»',
+		coordinates: [2029, 1378],
+	},
+	{
+		id: 30,
+		name: 'Тропа здоровья',
+		coordinates: [2269, 1352],
+	},
+	{
+		id: 31,
+		name: 'Можжевеловая роща',
+		coordinates: [1781, 1400],
+	},
+	{
+		id: 32,
+		name: 'Тропа здоровья',
+		coordinates: [1709, 1508],
+	},
+	{
+		id: 33,
+		name: 'Вилла «Маша»',
+		coordinates: [1929, 1682],
+	},
+	{
+		id: 34,
+		name: 'Крытый бассейн с морской водой',
+		coordinates: [1929, 1934],
+	},
+	{
+		id: 35,
+		name: 'Фитнес-центр',
+		coordinates: [1793, 1978],
+	},
+	{
+		id: 36,
+		name: 'Балийский сад',
+		coordinates: [1663, 2031],
+	},
+	{
+		id: 37,
+		name: 'Чайный домик',
+		coordinates: [1711, 2101],
+	},
+	{
+		id: 38,
+		name: 'Ресторан Breeze Gourmet',
+		coordinates: [1913, 2323],
+	},
+	{
+		id: 39,
+		name: 'Breeze Cafe',
+		coordinates: [1875, 2461],
+	},
+	{
+		id: 40,
+		name: 'Пирс «Медуза»',
+		coordinates: [1789, 2759],
+	},
+	// todo
+	{
+		id: 41,
+		name: 'Пирс «Медуза»',
+		coordinates: [1789, 2759],
+	},
+]
